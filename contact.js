4cf0c8d683d0b59aab327ed727bbb8c11edbd7ae
thumat0 <tngu379@gmail.com>
2020-12-18 19:49:03 -0700

    First commit

---
+++ contact.js
@@ -0,0 +1,9 @@
+// alert('Major Concept ALERT');
+// document.write('<h1>Hello World<h1>');
+// let myname="Heather"
+//     alert('myname');
+const myname="Heather";
+function hello(){
+    alert("hello world");
+    document.getElementById("hello").innerHTML= myname;
+}
---
+++ home.js
@@ -0,0 +1,17 @@
+// alert('Hello World');
+
+// document.body.innerHTML= '<h2>‘Hi’</h2>';
+// document.getElementById('bannertitle').innerHTML='potatoe chips';
+
+// /* class and id */
+// document.querySelector('#bannertitle').innerHTML='potato head.';
+
+// /* p */
+// let turnBlue = document.querySelector("p"); 
+//     turnBlue.style.color= 'blue';
+
+// /* body*/
+// document.body.style.backgroundColor="pink";
+
+
+
